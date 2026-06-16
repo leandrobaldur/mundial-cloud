af676c14faf2d8e4651261e15214f15bcf07891b
leandrobaldur <160677582+leandrobaldur@users.noreply.github.com>
2026-06-16 18:24:50 -0400

    actualizacion config.js

--- app/config.js
+++ app/config.js
@@ -2,9 +2,9 @@
 // Deja vacío para modo local (datos de ejemplo en la web).
 // Tras configurar, haz push para que el pipeline del Lab 2 actualice S3.
 
-const API_PARTIDOS = "";
-const API_PARTIDO_ID = "";
-const API_POLERAS = "";
+const API_PARTIDOS = "https://52rd6eyeme.execute-api.us-east-2.amazonaws.com/produccion/partidos";
+const API_PARTIDO_ID = "https://52rd6eyeme.execute-api.us-east-2.amazonaws.com/produccion/partido-001";
+const API_POLERAS = "https://52rd6eyeme.execute-api.us-east-2.amazonaws.com/produccion/poleras";
 
 // Ejemplo (reemplaza TU_API):
 // const API_PARTIDOS = "https://TU_API.execute-api.us-east-1.amazonaws.com/prod/partidos";
